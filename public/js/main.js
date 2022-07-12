0acdc27b9db58dab262d8222432c9f90367df0d3
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-07-12 15:02:59 +0100

    add delete logic for each habit

---
+++ public/js/main.js
@@ -0,0 +1,71 @@
+// main.js
+const deleteText = document.querySelectorAll('.fa-trash')
+
+Array.from(deleteText).forEach((element)=>{
+    element.addEventListener('click', deleteHabit)
+})
+
+
+
+async function deleteHabit(){
+    console.log("click")
+    const name = this.parentNode.childNodes[0].innerText
+    const habit = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('deleteHabit', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'name': name,
+                'habit': habit
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+
+// deleteButton.addEventListener('click', (_) => {
+//     fetch('/habits', {
+//         method: 'delete',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({
+//             name: 'Bad',
+//         }),
+//     })
+//         .then((res) => {
+//             if (res.ok) return res.json();
+//         })
+//         .then((response) => {
+//             if (response === 'No habit to delete') {
+//                 messageDiv.textContent = 'No habit to delete';
+//             } else {
+//                 window.location.reload(true);
+//             }
+//         })
+//         .catch(console.error);
+// });
+
+
+// update.addEventListener('click', (_) => {
+//     fetch('/habits', {
+//         method: 'put',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({
+//             name: 'Good',
+//             habit: '20 Sit ups',
+//         }),
+//     })
+//         .then((res) => {
+//             if (res.ok) return res.json();
+//         })
+//         .then((response) => {
+//             window.location.reload(true);
+//         });
+// });
--- server.js
+++ server.js
@@ -1,65 +1,53 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
+const PORT = 3000;
+require('dotenv').config();
 
-MongoClient.connect(
-    'mongodb+srv://joe:test@habits.9o0tigu.mongodb.net/?retryWrites=true&w=majority',
-    { useUnifiedTopology: true }
-)
-    .then((client) => {
-        console.log('Connected to Database');
-        const db = client.db('rampant-habit');
-        const habitCollection = db.collection('habits');
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'rampant-habit';
 
-        app.set('view engine', 'ejs');
-        app.use(bodyParser.urlencoded({ extended: true }));
-        app.use(express.static('public'));
-        app.use(bodyParser.json());
-
-        app.get('/', (req, res) => {
-            db.collection('habits')
-                .find()
-                .toArray()
-                .then((results) => {
-                    res.render('index.ejs', { habits: results });
-                })
-                .catch((error) => console.error(error));
-        });
-
-        app.post('/habits', (req, res) => {
-            habitCollection
-                .insertOne(req.body)
-                .then((result) => {
-                    res.redirect('/');
-                })
-                .catch((error) => console.error(error));
-        });
-
-        app.put('/habits', (req, res) => {
-            habitCollection
-                .findOneAndUpdate(
-                    { name: 'Bad' },
-                    {
-                        $set: {
-                            name: req.body.name,
-                            habit: req.body.quote,
-                        },
-                    },
-                    {
-                        upsert: true,
-                    }
-                )
-                .then(result => {
-                    res.json('Success')
-                })
-                .catch((error) => console.error(error));
-        });
-
-        
-
-        app.listen(3000, () => {
-            console.log('listening on 3000');
-        });
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`);
+        db = client.db(dbName);
     })
-    .catch((error) => console.error(error));
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    db.collection('habits').find().toArray()
+        .then(data => {
+            res.render('index.ejs', { info: data });
+        })
+        .catch(error => console.error(error));
+});
+
+app.post('/addHabit', (req, res) => {
+    db.collection('habits').insertOne(req.body)
+    .then((result) => {
+        console.log('Habit added')
+        res.redirect('/');
+    })
+        .catch((error) => console.error(error));
+});
+
+
+app.delete('/deleteHabit', (req, res) => {
+    db.collection('habits').deleteOne({ name: req.body.name })
+        .then((result) => {
+            console.log("Deleted habit")
+            res.json('Deleted habit');
+        })
+        .catch((error) => console.error(error));
+});
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
+
+// .catch((error) => console.error(error));
